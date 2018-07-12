69fc3cc1f6be70014ea728ec7b9589025e1b2fea
Runpeng L <5514270+runpengl@users.noreply.github.com>
2018-07-11 20:31:13 -0400

    je ne savais pas où aller

--- root-android-device/installTarget.sh
+++ root-android-device/installTarget.sh
@@ -1,6 +1,5 @@
 echo "====== [`date +%Y-%m-%d_%H:%M:%S`] Starting app installation process for device[$2] $1 ======" |& tee -a device_install.log
-for f in "${@:3}"
-do
+for f in "${@:3}"; do
         filename=$(basename $f)
 	echo "*** Installing $filename on device[$2] $1 ***" |& tee -a device_install.log
 	adb -s $1 install -r $f

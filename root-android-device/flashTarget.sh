507ff38311ee0157c09522f07d7abdfe8ad4b14d
Runpeng L <5514270+runpengl@users.noreply.github.com>
2018-07-12 05:51:52 -0400

    Update flashTarget.sh

--- root-android-device/flashTarget.sh
+++ root-android-device/flashTarget.sh
@@ -1,13 +1,16 @@
 # Unlocks, flashes, and reboots single target device 
 
-echo Starting to process device[$2] $1 at `date +%Y-%m-%d_%H:%M:%S` |& tee -a setup_device.log; 
-echo Unlocking device[$2] $1 |& tee -a setup_device.log; 
-fastboot -s $1 oem unlock; 
-fastboot -s $1 oem unlock; 
-echo Device[$2] $1 unlocked at `date +%Y-%m-%d_%H:%M:%S` |& tee -a setup_device.log; 
-fastboot flash boot boot.img -s $1;
-fastboot flash recovery recovery.img -s $1;
-fastboot flash system system.img -s $1; 
-echo Device[$2] $1 all partitions flashed at `date +%Y-%m-%d_%H:%M:%S` |& tee -a setup_device.log;
-fastboot reboot -s $1; 
-echo Device[$2] $1 completed if no errors logged here, at `date +%Y-%m-%d_%H:%M:%S` |& tee -a setup_device.log;
+echo "===  [`date +%Y-%m-%d_%H:%M:%S`] Starting to process device[$2] $1 ===" |& tee -a setup_device.log
+
+echo "*** Unlocking device[$2] $1 ***" |& tee -a setup_device.log
+fastboot -s $1 oem unlock 
+fastboot -s $1 oem unlock
+echo "Device[$2] $1 unlocked at `date +%Y-%m-%d_%H:%M:%S`" |& tee -a setup_device.log
+
+fastboot -s $1 flash boot $3boot.img
+fastboot -s $1 flash recovery $3recovery.img
+fastboot -s $1 flash system $3system.img 
+echo "Device[$2] $1 all partitions flashed at `date +%Y-%m-%d_%H:%M:%S`" |& tee -a setup_device.log
+
+fastboot -s $1 reboot 
+echo "=== [`date +%Y-%m-%d_%H:%M:%S] Device[$2] $1 completed if no errors logged here ===" |& tee -a setup_device.log;

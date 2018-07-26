ad5b2d22b0610d733abe334de200cf3a08ada159
Runpeng Liu <liu.runpeng1019@gmail.com>
2018-07-26 16:20:49 -0400

    Mais je ne savais pas ou allez

--- root-android-device/flashTarget.sh
+++ root-android-device/flashTarget.sh
@@ -3,14 +3,14 @@
 echo "===  [`date +%Y-%m-%d_%H:%M:%S`] Starting to process device[$2] $1 ===" |& tee -a setup_device.log
 
 echo "*** Unlocking device[$2] $1 ***" |& tee -a setup_device.log
-fastboot -s $1 oem unlock 
-fastboot -s $1 oem unlock
+sudo fastboot -s $1 oem unlock 
+sudo fastboot -s $1 oem unlock
 echo "Device[$2] $1 unlocked at `date +%Y-%m-%d_%H:%M:%S`" |& tee -a setup_device.log
 
-fastboot -s $1 flash boot $3boot.img
-fastboot -s $1 flash recovery $3recovery.img
-fastboot -s $1 flash system $3system.img 
+sudo fastboot -s $1 flash boot $3rooted_boot.img
+sudo fastboot -s $1 flash recovery $3recovery.img
+sudo fastboot -s $1 flash system $3system.img 
 echo "Device[$2] $1 all partitions flashed at `date +%Y-%m-%d_%H:%M:%S`" |& tee -a setup_device.log
 
-fastboot -s $1 reboot 
+sudo fastboot -s $1 reboot 
 echo "=== [`date +%Y-%m-%d_%H:%M:%S] Device[$2] $1 completed if no errors logged here ===" |& tee -a setup_device.log;

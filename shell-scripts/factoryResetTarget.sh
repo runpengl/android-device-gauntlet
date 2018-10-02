6c9b0f4b5a8daf82b07115f49d408a962e2cee46
Runpeng Liu <liu.runpeng1019@gmail.com>
2018-10-02 17:30:41 -0400

    Fix factoryReset and gauntletBootload

--- shell-scripts/factoryResetTarget.sh
+++ shell-scripts/factoryResetTarget.sh
@@ -1,3 +1,3 @@
 echo "====== [`date +%Y-%m-%d_%H:%M:%S`] Sending signal to factory reset device[$2] $1 ======" |& tee -a device_install.log
-adb -s $1 am broadcast -a android.intent.action.MASTER_CLEAR
+adb -s $1 shell am broadcast -a android.intent.action.MASTER_CLEAR
 

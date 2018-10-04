ecb93b1104dd41a825eefed103b067102caac0d7
Runpeng Liu <liu.runpeng1019@gmail.com>
2018-10-04 08:50:26 -0400

    Fix typo in flashTarget.sh

--- shell-scripts/flashTarget.sh
+++ shell-scripts/flashTarget.sh
@@ -13,4 +13,4 @@ sudo fastboot -s $1 flash system $3system.img
 echo "Device[$2] $1 all partitions flashed at `date +%Y-%m-%d_%H:%M:%S`" |& tee -a setup_device.log
 
 sudo fastboot -s $1 reboot 
-echo "=== [`date +%Y-%m-%d_%H:%M:%S] Device[$2] $1 completed if no errors logged here ===" |& tee -a setup_device.log;
+echo "=== [`date +%Y-%m-%d_%H:%M:%S`] Device[$2] $1 completed if no errors logged here ===" |& tee -a setup_device.log;
